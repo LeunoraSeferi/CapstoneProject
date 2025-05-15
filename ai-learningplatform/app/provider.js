31c7328ed5a8daa5d4905a23a989a6ac82f83327
LeunoraSeferi <ls30075@seeu.edu.mk>
2025-05-15 19:57:52 +0200

    Function Setup(Inngest)

--- ai-learningplatform/app/provider.js
+++ ai-learningplatform/app/provider.js
@@ -4,6 +4,8 @@ import {USER_TABLE} from '@/configs/schema';
 import { useUser } from '@clerk/nextjs'
 import {eq} from 'drizzle-orm';
 import React,{useEffect} from 'react'
+import axios from "axios";
+
 
 function Provider({ children }) {
 
@@ -15,24 +17,25 @@ function Provider({ children }) {
 
   const CheckIsNewUser = async() => {
     // Check Is User Already Exist
+    //const result = await db
+    //  .select()
+      //.from(USER_TABLE)
+     // .where(eq(USER_TABLE.email, user?.primaryEmailAddress?.emailAddress));
 
-    const result = await db
-      .select()
-      .from(USER_TABLE)
-      .where(eq(USER_TABLE.email, user?.primaryEmailAddress?.emailAddress));
-
-
-    console.log(result);
-    if(result?.length==0)
-    {
+   // if(result?.length==0)
+   // {
         //If not,then add to DB
-        const userResp=await db.insert(USER_TABLE).values({
-            name:user?.fullName,
-            email:user?.primaryEmailAddress?.emailAddress
-        }).returning({id:USER_TABLE.id})
+     //   const userResp=await db.insert(USER_TABLE).values({
+     //       name:user?.fullName,
+     //       email:user?.primaryEmailAddress?.emailAddress
+    //    }).returning({id:USER_TABLE.id})
 
-        console.log(userResp);
-    }
+   // }
+
+   const resp=await axios.post('/api/create-user',{user:user});
+   console.log(resp.data);
+
+   
   }
 
   return (
---
+++ ai-learningplatform/inngest/functions.js
@@ -0,0 +1,48 @@
+import { inngest } from "./client";
+import { db } from "@/configs/db";
+import { USER_TABLE } from "@/configs/schema";
+import { eq } from "drizzle-orm";
+
+        export const helloWorld = inngest.createFunction(
+        { id: "hello-world" },
+        { event: "test/hello.world" },
+        async ({ event, step }) => {
+            await step.sleep("wait-a-moment", "1s");
+            return { event, body: "Hello,World!" };
+        });
+
+        export const CreateNewUser = inngest.createFunction(
+            { id: "create-user" },
+            { event: "user.create" },
+            async ({ event, step }) => {
+                const {user} = event.data; 
+                const result = await step.run('Check User and create New if not in DB',async()=>{
+                                    // Check Is User Already Exist
+                const result = await db
+                .select()
+                .from(USER_TABLE)
+                .where(eq(USER_TABLE.email, user?.primaryEmailAddress?.emailAddress));
+
+                if (result?.length == 0) {
+                //If not,then add to DB
+                const userResp = await db
+                    .insert(USER_TABLE)
+                    .values({
+                    name: user?.fullName,
+                    email: user?.primaryEmailAddress?.emailAddress,
+                    })
+                    .returning({ id: USER_TABLE.id });
+                    return userResp;
+                }
+                return result;
+              })
+              return 'Success';
+            }
+             //Step is to Send Welcome Email notification
+
+             //Step to Send Email notification after 3 days once user join it
+        ) 
+    
+
+   
+ 
